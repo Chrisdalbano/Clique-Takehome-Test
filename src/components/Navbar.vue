2ec4abaf92aa9993b22cd7249edf8a4a8e26fb1c
Chrisdalb <chrisdalbano12@gmail.com>
2024-06-19 14:36:43 -0400

    first commit, added fonts, images, set up initial environment

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar">
+       
+    </nav>
+
+</template>
+
+<script lang="ts">
+export default defineComponent {
+    name: "Navbar",
+    setup() {
+        return{};
+    }
+}
+</script>
+
+<style></style>
